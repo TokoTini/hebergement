6c7653130b949f7359552afae401f1f530b3cf88
TokoTini <toko@n-py.com>
2023-04-11 16:15:09 +0200

    test

---
+++ js.js
@@ -0,0 +1,77 @@
+let slider = document.querySelector(".img-slider");
+let parentDiv = document.querySelector(".img-container")
+let imgs = slider.getElementsByTagName("img");
+let menuBtn = document.querySelector("#menu");
+let navbar = document.querySelector(".navList");
+let leftButtons = document.querySelectorAll('.btnLeft');
+let rightButtons = document.querySelectorAll('.btnRight');
+
+
+menuBtn.addEventListener('click', () => {
+    navbar.classList.toggle('active')
+})
+
+
+rightButtons.forEach(function(rightButton){
+    rightButton.addEventListener('click', function(event){
+        let parentDiv = event.target.closest('.img-container').querySelector('.img-slider');
+        parentDiv.scrollBy({
+            left: 300,
+            behavior: 'smooth'
+        })
+    })
+})
+
+
+
+leftButtons.forEach(function(leftButton){
+    leftButton.addEventListener('click', function(event){
+        let parentDiv = event.target.closest('.img-container').querySelector('.img-slider');
+        parentDiv.scrollBy({
+            // left: -parentDiv.offsetWidth,
+            left: -300,
+            behavior: 'smooth'
+        })
+    })
+})
+
+
+let stations = document.querySelector(".station");
+let dropdown = document.querySelector(".list-dropdown")
+stations.addEventListener('click', () => {
+    stations.classList.toggle('active')
+    dropdown.classList.toggle('active')
+})
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to('.select',{
+    scrollTrigger: {
+        trigger: ".lower",
+        start: "top 10%",
+        end: "bottom 75%",
+        toggleActions: "",
+        toggleClass: { className: "active", targets: ".select" },
+        markers: {
+            startColor: 'black',
+            endColor: "blue",
+            fontSize: '1.75rem',
+        },
+    }
+})
+
+gsap.to('.map',{
+    scrollTrigger: {
+        trigger: ".lower",
+        start: "top 10%",
+        end: "bottom 75%",
+        toggleActions: "",
+        toggleClass: { className: "active", targets: ".map" },
+        markers: {
+            startColor: 'yellow',
+            endColor: "pink",
+            fontSize: '1.75rem',
+        },
+    }
+})

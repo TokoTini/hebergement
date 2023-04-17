2cfaf358a38db8650a65af93f91b7213eb2bd4e0
TokoTini <129732627+TokoTini@users.noreply.github.com>
2023-04-17 17:07:31 +0200

    maj17/04

--- test/js.js
+++ test/js.js
@@ -1,11 +1,11 @@
-let mainList = document.querySelectorAll('.primary li');
+let mainList = document.querySelectorAll('.primary > li');
 
 mainList.forEach(li => {
     li.addEventListener('click', (e) => {
-        e.stopPropagation()
-        // mainList.forEach(function(item) {
-        //     item.classList.remove('active');
-        // })
+        // e.stopPropagation()
+        mainList.forEach(function(item) {
+            item.classList.remove('active');
+        })
         li.classList.toggle('active');
     })
 })
@@ -16,10 +16,36 @@ let secondaryList = document.querySelectorAll('.dropdown ul li')
 
 secondaryList.forEach(li => {
     li.addEventListener('click', (e) => {
-        e.stopPropagation()
+        // e.stopPropagation()
         secondaryList.forEach(function(item) {
             item.classList.remove('active');
         })
-        li.classList.add('active');
+        li.classList.toggle('active');
     })
+})
+
+
+let personnesInput = document.querySelector('.personnes .dropdown p')
+let personnes = document.querySelector('.personnes > p');
+let plusBtn = document.querySelector('.plus')
+let minusBtn = document.querySelector('.minus')
+
+
+
+let n = 4;
+plusBtn.addEventListener('click', () => {
+    n++;
+    personnesInput.innerHTML = n +" " + "Personnes";
+    personnes.innerHTML = n + " " + "Personnes";
+})
+
+minusBtn.addEventListener('click', () => {
+    if(n > 1){
+        n--;
+    }else{
+        n = 1;
+    }
+    let person = n > 1 ? "Personnes" : "Personne";
+    personnesInput.innerHTML = n +" "+ person;
+    personnes.innerHTML = n +" "+ person;
 })
